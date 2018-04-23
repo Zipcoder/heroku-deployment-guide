795402bb4523a87f6a984f58409c080859c5d56e
miscbits <willow6786@gmail.com>
2018-04-23 17:39:09 -0400

    removed database

--- src/main/java/com/zipcodewilmington/heroku_demo/Controller.java
+++ src/main/java/com/zipcodewilmington/heroku_demo/Controller.java
@@ -1,12 +1,8 @@
 package com.zipcodewilmington.heroku_demo;
 
-import org.springframework.util.ClassUtils;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class Controller {
@@ -16,11 +12,4 @@ public class Controller {
     public String index() {
         return "Hello World";
     }
-
-
-    @ExceptionHandler(Throwable.class)
-    public String handleAnyException(Throwable ex, HttpServletRequest request) {
-        return ClassUtils.getShortName(ex.getClass());
-    }
-
 }
--- src/main/java/com/zipcodewilmington/heroku_demo/ItemDAO.java
+++
@@ -1,6 +0,0 @@
-package com.zipcodewilmington.heroku_demo;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface ItemDAO extends PagingAndSortingRepository<Long, Item> {
-}
