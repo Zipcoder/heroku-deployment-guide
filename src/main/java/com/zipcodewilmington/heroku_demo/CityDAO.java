2d6fac0fee67eb4f628a7f682de46629d3186fc5
miscbits <willow6786@gmail.com>
2018-04-24 17:06:33 -0400

    added rest repo annotation

--- src/main/java/com/zipcodewilmington/heroku_demo/CityDAO.java
+++ src/main/java/com/zipcodewilmington/heroku_demo/CityDAO.java
@@ -2,7 +2,9 @@ package com.zipcodewilmington.heroku_demo;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @Repository
+@RepositoryRestResource(collectionResourceRel = "cites", path = "cities")
 public interface CityDAO extends PagingAndSortingRepository<City, Long> {
 }
