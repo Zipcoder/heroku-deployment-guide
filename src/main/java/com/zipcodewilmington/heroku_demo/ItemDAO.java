3b834f3d563a92b21a09836f351995a355ca82d7
miscbits <willow6786@gmail.com>
2018-04-24 16:21:45 -0400

    @repo

--- src/main/java/com/zipcodewilmington/heroku_demo/ItemDAO.java
+++ src/main/java/com/zipcodewilmington/heroku_demo/ItemDAO.java
@@ -1,6 +1,8 @@
 package com.zipcodewilmington.heroku_demo;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemDAO extends PagingAndSortingRepository<Long, Item> {
 }
