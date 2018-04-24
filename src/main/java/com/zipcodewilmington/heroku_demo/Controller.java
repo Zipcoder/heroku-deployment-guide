c97ddf8ebbad681d19d8525a0d561f4435b333dc
miscbits <willow6786@gmail.com>
2018-04-24 17:33:01 -0400

    added cities route

--- src/main/java/com/zipcodewilmington/heroku_demo/Controller.java
+++ src/main/java/com/zipcodewilmington/heroku_demo/Controller.java
@@ -1,5 +1,6 @@
 package com.zipcodewilmington.heroku_demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.ClassUtils;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +12,20 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class Controller {
 
+    @Autowired
+    CityDAO cityDAO;
+
     @RequestMapping("/")
     @ResponseBody
     public String index() {
         return "Hello World";
     }
 
+    @RequestMapping("/cities")
+    @ResponseBody
+    public Iterable<City> cities() {
+        return cityDAO.findAll();
+    }
 
     @ExceptionHandler(Throwable.class)
     public String handleAnyException(Throwable ex, HttpServletRequest request) {
