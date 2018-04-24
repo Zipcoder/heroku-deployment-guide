928288bc44defba7614d6e3be09e01339e1f9db6
miscbits <willow6786@gmail.com>
2018-04-24 17:08:38 -0400

    fixed typo

--- src/main/java/com/zipcodewilmington/heroku_demo/CityDAO.java
+++ src/main/java/com/zipcodewilmington/heroku_demo/CityDAO.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @Repository
-@RepositoryRestResource(collectionResourceRel = "cites", path = "cities")
+@RepositoryRestResource(collectionResourceRel = "cities", path = "cities")
 public interface CityDAO extends PagingAndSortingRepository<City, Long> {
 }
